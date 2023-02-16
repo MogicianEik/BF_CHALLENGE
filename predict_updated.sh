e9562f3da9071645bba5e28b97a9162fea64797f
Eik <yiz370@ucsd.edu>
2023-02-16 16:34:52 -0500

    Branch Update: add single-decoder branch
    single decoder branch predicts only 3 classes:
    0 - 2 copies of H1B1G1
    1 - 1 copy
    2 - 0 copy

--- predict_updated.sh
+++ predict_updated.sh
@@ -1,10 +1,10 @@
 export CUDA_VISIBLE_DEVICES=0
 python -W ignore src/main.py \
---n_class 9 \
+--n_class 3 \
 --pred_file "data/Prediction_data_updated.tbx" \
---model_path "outputs/saved_models/updated_set_best0.69_fold3/" \
+--model_path "outputs/saved_models/" \
 --log_path "outputs/runs/" \
---task_name "SV_predict_updated" \
+--task_name "SV_SD_predict_updated" \
 --batch_size 1 \
 --prediction \
---pred_rotation 3
+--pred_rotation 1
